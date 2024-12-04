c7faca3cdb0de5d1e507d11ad79951c6ac79c9d0
Gabriel Vinicios <crisishd2332@gmail.com>
2024-12-04 12:04:00 -0300

    Update Kotlin DL version to 0.5.2 in build script
    This commit updates the Kotlin DL library from a version placeholder to a specific version 0.5.2 in the build.gradle.kts file. This change ensures compatibility and stability by aligning the project with a stable release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml-jvm")
-    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-api:[KOTLIN-DL-VERSION]")
+    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-api:0.5.2")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
